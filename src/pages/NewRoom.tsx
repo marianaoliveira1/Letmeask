d794d0aacde0aca7c4b51eac8af1e76038965251
marianaoliveirafernandes@alu.ufc.br <marianaoliveirafernandes@alu.ufc.br>
2022-04-04 11:04:07 -0300

    rotas na tela de home criacao de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
+import {Link} from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
@@ -23,7 +24,7 @@ export function NewRoom() {
                        />
                        <Button type='submit'>Criar sala</Button>
                     </form>
-                    <p>Quer entrar em uma sala existente? <a href='#'>Clique aqui</a></p>
+                    <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
                 </div>
             </main>
         </div>
