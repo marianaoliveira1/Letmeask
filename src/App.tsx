4377aa7256b8fb877484491969bac304ac4dc5cd
marianaoliveirafernandes@alu.ufc.br <marianaoliveirafernandes@alu.ufc.br>
2022-04-08 11:28:58 -0300

    sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 function App() {
   return (
     <BrowserRouter>
         <AuthContextProvider>
           <Route path="/" exact component={Home} />
-        <Route path="/rooms/news" component={NewRoom} />
+        <Route path="/rooms/news" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
         </AuthContextProvider>
     </BrowserRouter>
   );
